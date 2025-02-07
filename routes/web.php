3d9fca3828abf2a5e5b790cad9147ea25fad7f0e
Vijay <vijay19971112@gmail.com>
2025-02-07 15:56:33 +0530

    this add new roure

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,7 @@ Route::get('/refund-and-cancelation-policy', function () {
     return view('refund-and-cancelation-policy');
 });
 
-
+//kkdkhdhdhdjdhfdf 
 
 
 Route::middleware([AuthCheck::class])->group(function () {
